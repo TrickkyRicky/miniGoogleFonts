9148e0564252428f65964550e8ea6ddad97b675e
trickkyricky <rickygyoungjr@gmail.com>
2023-05-07 19:25:04 -0500

    added packages

---
+++ src/globalStyle.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+const CreateGlobal = createGlobalStyle`
+
+:root {
+  --typography-1: #ccd6f6;
+  --scroll-dark-1: #212323;
+  --scroll-dark-2: #333333;
+  --black: #000000;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  
+  html {
+    scroll-behavior: smooth;
+    scrollbar-width: thin;
+    scrollbar-color: var(--scroll-dark-2) var(--scroll-dark-1);
+    letter-spacing: .01px;
+  }
+  ::-webkit-scrollbar {
+    width: 12px;
+  }
+  ::-webkit-scrollbar-track {
+    background: var(--scroll-dark-1);
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--scroll-dark-2);
+    border: 3px solid var(--scroll-dark-1);
+    border-radius: 10px;
+  }
+  
+  body {
+    width: 100%;
+    height: 300vh;
+    background-color: darkblue;
+  }
+
+`;
+
+export default CreateGlobal;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import CreateGlobal from "./globalStyle.ts";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <CreateGlobal />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
